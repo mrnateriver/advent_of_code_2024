f5574d168597a07f01c4af04b9ac8b5b023465d6
Evgenii Dobrovidov <mr.nate.river@gmail.com>
2025-01-02 18:46:44 +0100

    Add set operations

--- shared/maps.go
+++ shared/maps.go
@@ -7,3 +7,24 @@ func Keys[K comparable, V any](m map[K]V) []K {
 	}
 	return keys
 }
+
+func Union[K comparable, V any](a, b map[K]V) map[K]V {
+	res := make(map[K]V, max(len(a), len(b)))
+	for k, v := range a {
+		res[k] = v
+	}
+	for k, v := range b {
+		res[k] = v
+	}
+	return res
+}
+
+func Intersect[K comparable, V any](a, b map[K]V) map[K]V {
+	res := make(map[K]V, min(len(a), len(b)))
+	for k := range a {
+		if _, exists := b[k]; exists {
+			res[k] = a[k]
+		}
+	}
+	return res
+}
